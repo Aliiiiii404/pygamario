9606a0229f154eea626da9777047012f49825cbe
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-04 15:03:44 +0100

    the tilesets in Tiled

---
+++ maps/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain" tilewidth="16" tileheight="16" tilecount="242" columns="22">
+ <image source="adventure/Terrain/Terrain (16x16).png" trans="55aaff" width="352" height="176"/>
+</tileset>

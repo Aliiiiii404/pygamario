7cc23c44e4efaf77188680a9b1087ca33889ad00
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-05 22:04:33 +0100

    replacing the old backgound image

--- maps/background.tsx
+++ maps/background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="background" tilewidth="16" tileheight="16" tilecount="18" columns="6">
- <image source="backgrounds/Clouds_Loopable_100x59.png" trans="000000" width="100" height="59"/>
+ <image source="backgrounds/Clouds_Loopable_100x59.png" trans="55aaff" width="100" height="59"/>
 </tileset>

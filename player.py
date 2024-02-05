65f1013f845ac135ee7547439841f23adaaea7f9
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-05 21:58:06 +0100

    new: adding gravity, changing the way collision is handled and implementing spreet sheets for running and jumping animation

--- player.py
+++ player.py
@@ -1,67 +1,134 @@
+from itertools import filterfalse
 from time import sleep
 import pygame
+
 
 class Player(pygame.sprite.Sprite):
         def __init__(self, x, y):
             super().__init__()
-            self.sprite_sheet = pygame.image.load('./maps/adventure/main-characters/mask-dude/run.png')
+            # set the sprite sheet to run animation
+            self.set_sprite_sheet('run.png')
+            self.frame_index = 0
+            self.last_frame_time = pygame.time.get_ticks()
+            # Set the duration for each frame
+            self.duration = 50
             self.image = self.get_image(0, 0)
             self.image.set_colorkey((0, 0, 0))
             self.rect = self.image.get_rect()
             self.position = [x, y]
-            self.images = {
-                'down' : self.get_image(4, 0),
-                'left' : pygame.transform.flip(self.get_image(0, 0), True, False),
-                'right' : self.get_image(0, 0),
-                'up' : self.get_image(4, 0),
-            }
+            # the player's speed
+            self.speed = 3
+            self.jump_height = 5
+            # the player's direction 1 is right, -1 is left
+            self.direction = 1
+            # the single jump
+            self.singl_jump = 1
+            # the player's feet to detect collision
             self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
             self.old_position = self.position.copy()
-            self.speed = 3
-            self.jump_height = 6
-            self.jump_count = 0
-            self.jumping = False
-            self.jump_in_progress = False
-        def save_location(self):
-            self.old_position = self.position.copy()
-        
-        def change_image(self, name):
-            self.image = self.images[name]
+        # Set the sprite sheet
+        def set_sprite_sheet(self, sheet_name):
+            self.sprite_sheet = pygame.image.load(f'./maps/adventure/main-characters/mask-dude/{sheet_name}')
+        # Movment
+        def move_right(self):
+            self.direction = 1
+            self.position[0] += self.speed
+            self.animate_run()
+        def move_left(self): 
+            self.direction = -1
+            self.position[0] -= self.speed
+            self.animate_run()
+        def move_down(self):
+            self.position[1] += self.speed
+        def jump(self, gravity_zones):
+            self.position[1] -= self.jump_height
+            self.animate_jump()
+                
+        def double_jump(self):
+            print("code for double jump didnt go brrrrrrrrr ;(")
+
+        # Movment animation
+        def animate_run(self):
+            # Update frame index for animation
+            self.set_sprite_sheet('run.png')
+            current_time = pygame.time.get_ticks()
+            if current_time - self.last_frame_time > self.duration:
+                self.frame_index = (self.frame_index + 1) % 12
+                self.last_frame_time = current_time
+            # Update image based on frame index and direction
+            frame_offset = self.frame_index * 32
+            if self.direction == 1:
+                self.image = self.get_image(frame_offset, 0)
+            elif self.direction == -1:
+                # Flip the image horizontally for left movement
+                self.image = pygame.transform.flip(self.get_image(frame_offset, 0), True, False)
             self.image.set_colorkey((0, 0, 0))
-        # Movement methods
-        def move_right(self): self.position[0] += self.speed
-        def move_left(self): self.position[0] -= self.speed
-        def move_up(self): self.position[1] -= self.speed
-        def jump(self):
-            if not self.jumping:
-                self.jumping = True
-                self.jump_count = self.jump_height
-                self.jump_in_progress = True
+            self.rect = self.image.get_rect(topleft=self.position)
 
-            if self.jump_in_progress:
-                neg = 1
-                if self.jump_count < 0:
-                    neg = -1
+        def animate_jump(self):
+            self.set_sprite_sheet('jump.png')
+            self.frame_index = (self.frame_index + 1) % 1  # Assuming 6 frames for jumping animation
+            frame_offset = 0
+            if self.direction == 1:
+                self.image = self.get_image(frame_offset, 0)
+            elif self.direction == -1:
+                self.image = pygame.transform.flip(self.get_image(frame_offset, 0), True, False)
+            self.image.set_colorkey((0, 0, 0))
+            self.rect = self.image.get_rect(topleft=self.position)
 
-                self.position[1] -= (self.jump_count ** 2) * 0.8 * neg
-                self.jump_count -= 1
-                print("Jumping")
-                if self.jump_count < -self.jump_height:
-                    self.jumping = False
-                    self.jump_in_progress = False
-                    self.jump_count = self.jump_height
-        
-        def move_down(self): self.position[1] += self.speed
-        
-        def update(self):
-            self.rect.topleft = self.position
-            self.feet.midbottom = self.rect.midbottom
+        def animate_double_jump(self):
+            self.set_sprite_sheet('double-jump.png')
+            current_time = pygame.time.get_ticks()
+            if current_time - self.last_frame_time > self.duration:
+                self.frame_index = (self.frame_index + 1) % 6  # Assuming 6 frames for jumping animation
+                self.last_frame_time = current_time
+            frame_offset = self.frame_index * 32
+            if self.direction == 1:
+                self.image = self.get_image(frame_offset, 0)
+            elif self.direction == -1:
+                self.image = pygame.transform.flip(self.get_image(frame_offset, 0), True, False)
+            self.image.set_colorkey((0, 0, 0))
+            self.rect = self.image.get_rect(topleft=self.position)
+
+        def save_location(self): 
+            self.old_position = self.position.copy()
+
         def move_back(self):
             self.position = self.old_position
             self.rect.topleft = self.position
             self.feet.midbottom = self.rect.midbottom
+
+        def get_position(self):
+            return self.rect
+
+        def gravity(self, gravity_zones):
+            default_gravity = 1.5
+            gravity_applied = True
+            # Check for gravity zones
+            for zone in gravity_zones:
+                zone_rect = pygame.Rect(zone.x, zone.y, zone.width, zone.height)
+                if zone_rect.colliderect(self.rect):
+                    gravity_applied = False
+                    # whene the player touches the ground switch back to the run animation sprite sheet
+                    self.set_sprite_sheet('run.png')
+                    break
+                else:
+                    gravity_applied = True
+            # if no collision with gravity zones, apply default gravity
+            if gravity_applied:
+                self.position[1] += default_gravity
+
+        def update(self):
+            self.rect.topleft = self.position
+            self.feet.midbottom = self.rect.midbottom
+            if self.frame_index == 7:  # Assuming 8 frames, so index 7 is the last frame
+                self.is_jumping = False
+                # Switch back to the run animation sprite sheet
+                self.set_sprite_sheet('run.png')
+
         def get_image(self, x, y):
-            image = pygame.Surface([32, 32])
-            image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
-            return image
+           image = pygame.Surface([32, 32])
+           image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
+           return image
+
 

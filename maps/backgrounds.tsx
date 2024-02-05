8999f4a22a2cd9ea0586e1c8ede5f6c49b039af4
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-05 22:06:11 +0100

    importing the background combined images to Tiled

---
+++ maps/backgrounds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="backgrounds" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="adventure/Background/combine_images.png" trans="55aaff" width="192" height="192"/>
+</tileset>

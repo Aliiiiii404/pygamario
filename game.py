2e635426582b7d8507d89c8a164acc5251237f05
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-05 22:00:53 +0100

    new: importing the gravity from tha Player class, and checking if player reached checkpoint

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pytmx
 import pyscroll
 from player import Player
 
+
 # Game class
 class Game:
     # Constructor
@@ -16,45 +17,56 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame("maps/map.tmx")
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        # font
+        self.font = pygame.font.Font(None, 36)
         # create player
         player_position = tmx_data.get_object_by_name("player_spawn")
         self.player = Player(player_position.x, player_position.y)
-        # collision detection
-        self.walls = []
-        for obj in tmx_data.objects:
-            if obj.type == "collision":
-                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
         # create map layers
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
         self.group.add(self.player)
-    
+        # checkpoint text
+        self.checkpoint_text = tmx_data.get_object_by_name("checkpointText")
+        self.checkpoint_text.text = "test"
+        self.checkpoint_text.visible = 1
+        # gravity zones
+        self.gravity_zones = []
+        self.collision_zones = []
+        self.checkpoint_position = []
+        for obj in tmx_data.objects:
+            if obj.type == "GravityZones":
+                self.gravity_zones.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            if obj.type == "collision":
+                self.collision_zones.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            if obj.type == "checkpoint":
+                self.checkpoint_position = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
     # Handle input
     def handle_input(self):
         pressed = pygame.key.get_pressed()
         # handle movement
-        if pressed[pygame.K_DOWN]:
+        if pressed[pygame.K_SPACE]:
+            self.player.jump(self.gravity_zones)
+        elif pressed[pygame.K_DOWN]:
             self.player.move_down()
-            self.player.change_image('down')
         elif pressed[pygame.K_LEFT]:
             self.player.move_left()
-            self.player.change_image('left')
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
-            self.player.change_image('right')
-        # handle jump
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.player.jump()
-                    self.player.change_image('up')
-                    self.player.update()
+
+    # Check if checkpoint is reached
+    def checkpoint_reached(self, checkpoint_position):
+        player_position = self.player.get_position()
+        if player_position.colliderect(checkpoint_position):
+            self.checkpoint_text.visible = 0
+            print("ur here")
+    # update
     def update(self):
         self.group.update()
-        # check for collision
+        self.player.gravity(self.gravity_zones)
         for sprite in self.group.sprites():
-            if sprite.feet.collidelist(self.walls) > -1:
-                sprite.move_back()
-
+            if sprite.feet.collidelist(self.collision_zones) > -1:
+                self.player.move_back()
+        self.checkpoint_reached(self.checkpoint_position)
     # Run until the user asks to quit
     def run(self):
         clock = pygame.time.Clock()
@@ -70,3 +82,7 @@ class Game:
                     running = False
             clock.tick(60)
         pygame.quit()
+
+
+
+

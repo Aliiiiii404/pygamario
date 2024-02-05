9a68d19ee2cbe2be1ff8fd2a9817c3627519735a
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-05 22:06:41 +0100

    importing the checkpoints images to Tiled

---
+++ maps/checkpoint-moving.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="checkpoint-moving" tilewidth="16" tileheight="16" tilecount="416" columns="104">
+ <image source="adventure/Items/Checkpoints/Checkpoint/checkpoint-moving.png" width="1664" height="64"/>
+</tileset>

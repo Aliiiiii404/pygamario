52d83d639721141659012060dcaa280111f34bcb
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-06 14:25:26 +0100

    new: text colors for tiled

---
+++ maps/text-white.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="text-white" tilewidth="16" tileheight="16" tilecount="15" columns="5">
+ <image source="adventure/Menu/Text/Text (White) (8x10).png" width="80" height="50"/>
+</tileset>

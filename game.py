44c2f63b671b63ba8c45f9f86f25903bd8cc1eab
Ali Bencheikh <alibencheikh212@gmail.com>
2024-02-06 02:31:50 +0100

    new: a function to change level if checkpoint is reached

--- game.py
+++ game.py
@@ -14,21 +14,20 @@ class Game:
         # the window title
         pygame.display.set_caption("PygaMario")
         # load data from pytmx
-        tmx_data = pytmx.util_pygame.load_pygame("maps/map.tmx")
+        tmx_data = pytmx.util_pygame.load_pygame("maps/map-1.tmx")
         map_data = pyscroll.data.TiledMapData(tmx_data)
-        self.map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
         # font
         self.font = pygame.font.Font(None, 36)
         # create player
         player_position = tmx_data.get_object_by_name("player_spawn")
         self.player = Player(player_position.x, player_position.y)
+        # set level
+        self.level = 1
         # create map layers
-        self.group = pyscroll.PyscrollGroup(map_layer=self.map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
         self.group.add(self.player)
-        # checkpoint text
-        self.checkpoint_text = tmx_data.get_object_by_name("checkpointText")
-        self.checkpoint_text.text = "test"
-        self.checkpoint_text.visible = 1
         # gravity zones
         self.gravity_zones = []
         self.collision_zones = []
@@ -50,17 +49,36 @@ class Game:
             self.player.move_left()
         elif pressed[pygame.K_RIGHT]:
             self.player.move_right()
-        elif pressed[pygame.K_RETURN] or pressed[pygame.K_KP_ENTER]:
-            # zoom on the player
-            if self.map_layer.zoom != 2:
-                self.map_layer.zoom = 2
-
+    # Change level 
+    def change_level(self):
+        tmx_data = pytmx.util_pygame.load_pygame("./maps/map-2.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        map_layer.zoom = 2
+        # update collision zones 
+        self.gravity_zones = []
+        self.collision_zones = []
+        self.checkpoint_position = []
+        for obj in tmx_data.objects:
+            if obj.type == "GravityZones":
+                self.gravity_zones.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            if obj.type == "collision":
+                self.collision_zones.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+            if obj.type == "checkpoint":
+                self.checkpoint_position = pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+        # add player 
+        player_position = tmx_data.get_object_by_name("player_spawn")
+        self.player = Player(player_position.x, player_position.y)
+        self.group.add(self.player)
+    
     # Check if checkpoint is reached
     def checkpoint_reached(self, checkpoint_position):
         player_position = self.player.get_position()
         if player_position.colliderect(checkpoint_position):
-            self.checkpoint_text.visible = 0
-            print("ur here")
+            self.change_level()
+            print("Checkpoint reached")
+            
     # update
     def update(self):
         self.group.update()
